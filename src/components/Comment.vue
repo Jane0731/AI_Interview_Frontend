e3f36ed63a5a8a8a8fef9a78abd8314ceb6f07a3
jane0731 <jane991680420@outlook.com>
2023-07-23 21:13:29 +0800

    add comment component

---
+++ src/components/Comment.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-sheet rounded="lg" width="100%" class="px-4 mx-auto my-5">
+        <div class="d-flex flex-row align-center ">
+            <v-sheet>
+                <v-avatar color="brown">
+                    <span class="text-h5">aa</span>
+                </v-avatar>
+            </v-sheet>
+            <v-sheet class="ml-2 mr-4">
+                <div class="text-body-1">{{ comment.author.name }}</div>
+            </v-sheet>
+        </div>
+        <div class="text-h6 ">
+            <v-sheet>
+                {{ comment.comment }}
+            </v-sheet>
+        </div>
+        <div class="text-body-2 ">
+            {{ comment.update_at }} </div>
+    </v-sheet>
+</template>
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+    comment: { type: Object },
+})
+</script>
--- src/views/SingleDiscussion.vue
+++ src/views/SingleDiscussion.vue
@@ -5,20 +5,21 @@
                 <v-spacer></v-spacer>
             </v-col>
             <v-col cols="6">
-                <v-sheet rounded="lg" width="100%" class="pa-4 mx-auto my-5">
-                    <div class="d-flex flex-row align-center ma-2 pa-2">
-                        <v-avatar color="brown">
+                <v-sheet rounded="t-lg" width="100%">
+                    <div class="pa-4 mx-auto my-5" style="min-height:calc(100vh - 150px)">
+                        <div class="d-flex flex-row align-center ma-2 pa-2">
+                            <v-avatar color="brown">
                                 <span class="text-h5">女</span>
-                        </v-avatar>
+                            </v-avatar>
                             <div class="text-body-1 ml-2 mr-4">{{ discussion.poster }}</div>
-                    </div>
-                    <div class="text-h6 ma-2 pa-2">
+                        </div>
+                        <div class="text-h6 ma-2 pa-2">
                             {{ discussion.title }}
-                    </div>
-                    <div class="d-flex flex-row align-center ma-2 pa-2">
+                        </div>
+                        <div class="d-flex flex-row align-center ma-2 pa-2">
                             <div class="text-body-1 mr-4">{{ discussion.classification }}</div>
                             <div class="text-body-2">{{ discussion.posting_time }}</div>
-                    </div>
+                        </div>
                         <div class="text-body-2 ma-2 pa-2">{{ discussion.content }} </div>
                         <div class="ma-2 pa-2">
                             <v-chip-group selected-class="text-primary" column>
@@ -26,8 +27,8 @@
                                     {{ tag }}
                                 </v-chip>
                             </v-chip-group>
-                    </div>
-                    <div class="d-flex flex-row align-center ma-2 pa-2">
+                        </div>
+                        <div class="d-flex flex-row align-center ma-2 pa-2">
                             <v-btn prepend-icon="mdi-heart" variant="text">
                                 <template v-slot:prepend>
                                     <v-icon color="error"></v-icon>
@@ -40,6 +41,11 @@
                                 </template>
                                 {{ discussion.comments }}
                             </v-btn>
+                        </div>
+                        <v-divider></v-divider>
+
+                        <Comment v-for="comment in comments" :comment="comment" />
+                    </div>
                     <div class="comment-block d-flex">
                         <v-row justify="space-between" width="100%">
                             <div class="d-flex flex-row align-center pl-10 ">
@@ -62,8 +68,8 @@
 </template>
   
 <script setup>
-import { ref } from 'vue'
 import Keywords from '@/components/Keywords.vue'
+import Comment from '@/components/Comment.vue'
 const discussion = {
     "discussion_id": 1,
     "poster": "Jane",
@@ -75,20 +81,25 @@ const discussion = {
     "comments": 20,
     "tags": ["面試穿搭", "dfg"]
 }
+const comments = [
     {
-        id: 1,
-        iconName: "mdi-heart",
-        iconColor: "error"
+        "id": 1,
+        "author": {
+            "id": 1,
+            "name": "RJ"
+        },
+        "comment": "ㄚㄚㄚㄚㄚ",
+        "update_at": "2020-09-07T05:31:09.000000Z"
+
     },
     {
-        id: 2,
-        iconName: "mdi-comment",
-        iconColor: "primary"
-    },
-    {
-        id: 3,
-        iconName: "mdi-bookmark",
-        iconColor: "warning"
+        "id": 2,
+        "author": {
+            "id": 2,
+            "name": "Jane"
+        },
+        "comment": "ㄚㄚㄚㄚㄚ",
+        "update_at": "2020-09-07T05:31:09.000000Z"
     },
 ]
 </script>
