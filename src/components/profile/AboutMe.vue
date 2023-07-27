c0aa12135cdd4a85c72d5365a1517e91daca499c
jane0731 <jane991680420@outlook.com>
2023-07-27 15:01:34 +0800

    Add Profile's aboutme & favirites & posts & interviewRecords

---
+++ src/components/profile/AboutMe.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-card width="100%" class="overflow-auto">
+        <v-sheet width="100%" color="primary" height="120" class="d-flex align-center justify-space-between pa-10">
+            <div class="d-flex align-center">
+                <v-avatar color="brown">
+                    <span class="text-h5">aa</span>
+                </v-avatar>
+                <div class="text-h5  mx-3 pa-6">Hi~賊賊</div>
+            </div>
+            <v-btn variant="flat" color="#B6B6B6" size="large" class="text-h5 mx-3 pa-auto">
+                <div>變更名稱</div>
+            </v-btn>
+
+        </v-sheet>
+        <v-card-text>
+            <div class="ma-2 pa-2 text-h6">
+                基本資料
+            </div>
+            <div class="d-flex flex-row text-h6">
+                <div class="ma-2 pa-2">姓名</div>
+                <div class="my-2  mx-8 pa-2">劉賊賊</div>
+            </div>
+            <div class="d-flex flex-row text-h6">
+                <div class="ma-2 pa-2">姓名</div>
+                <div class="my-2  mx-8 pa-2">劉賊賊</div>
+            </div>
+            <div class="d-flex flex-row text-h6">
+                <div class="ma-2 pa-2">姓名</div>
+                <div class="my-2  mx-8 pa-2">劉賊賊</div>
+            </div>
+            <div class="d-flex flex-row text-h6">
+                <div class="ma-2 pa-2">姓名</div>
+                <div class="my-2  mx-8 pa-2">劉賊賊</div>
+            </div>
+            <div class="d-flex flex-row justify-end ">
+                <v-btn variant="flat" color="#B6B6B6" size="large" class="text-h5 mx-3 pa-auto">
+                    <div>修改資料</div>
+                </v-btn>
+            </div>
+            <v-divider class="my-6"></v-divider>
+            <div class="ma-2 pa-2 text-h6">
+                綁定通知
+            </div>
+            <div class="d-flex flex-row text-h6">
+                <div class="ma-2 pa-2">姓名</div>
+                <div class="my-2  mx-8 pa-2">劉賊賊</div>
+            </div>
+            <v-divider class="my-6"></v-divider>
+
+        </v-card-text>
+    </v-card>
+</template>
